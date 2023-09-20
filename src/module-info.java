263b15031b01f923485f1a9038300aa157aaf76b
Juann6969 <126709623+Juann6969@users.noreply.github.com>
2023-09-20 19:33:44 +0200

    Creado el metodo de arranque del sistema

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author JuanManuelGarcíaDíaz
+ *
+ */
+module ññ {
+}
---
+++ src/ññ/xx.java
@@ -0,0 +1,10 @@
+package ññ;
+
+public class xx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
